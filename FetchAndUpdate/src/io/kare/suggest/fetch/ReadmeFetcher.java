56232dd4766aa36599a5d3f8ffc9aa9969ceca5e
Adrian Chmielewski-Anders <adrian@h7.home>
2014-03-23 20:20:36 -0700

    adding the basic readme fetcher and keyword class

---
+++ FetchAndUpdate/src/io/kare/suggest/fetch/ReadmeFetcher.java
@@ -0,0 +1,112 @@
+package io.kare.suggest.fetch;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import io.kare.suggest.Logger;
+import io.kare.suggest.readmes.ReadmeCorrelations;
+import io.kare.suggest.readmes.ReadmeOptionPair;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author Adrian Chmielewski-Anders
+ * @since 0.0.1
+ */
+
+public class ReadmeFetcher {
+    // github has many ways of accessing the raw readmes. This is one
+    // https://github.com/user/repo/raw/branch/file.extension
+    // is another, but this one redirects ^^ to raw.github.com or
+    // raw.githubusercontent.com... weird
+    private static final String README_BASE_URL = "https://raw.github.com/";
+
+    /**
+     * list of all possible readmes because they are different repo to repo
+     * i've tried to add them all in order of popularity, if there is a better
+     * way around this let me know
+     */
+    private static final String[] POSSIBLE_READMES = {
+            "README.md",
+            "README.markdown",
+            "README.txt",
+            "README",
+            "README.rst",
+            "readme.md"
+    };
+
+    public static void fetch(DB db) {
+
+        DBCollection repos   = db.getCollection("repos"),
+                     readmes = db.getCollection("readmes");
+        ExecutorService exec = Executors.newFixedThreadPool(8);
+
+        DBCursor repoCursor = repos.find();
+        try {
+            while (repoCursor.hasNext()) {
+                exec.submit(() -> {
+                    BasicDBObject repoObject = (BasicDBObject) repoCursor.next();
+                    String url = getURL(repoObject);
+                    // check if we've already indexed this readme, avoid having to try
+                    // different possible readme options
+                    DBCursor readmeCursor = readmes.find(new BasicDBObject("name", repoObject.get("name")));
+                    if (readmeCursor.size() > 0) {
+                        BasicDBObject readmeObject = (BasicDBObject) readmeCursor.next();
+                        String readme = easyGet(url + readmeObject.get("readme_name"));
+                        readmeObject.append("readme", ReadmeCorrelations.getCorrelations(readme));
+                        readmes.save(readmeObject);
+                    } else {
+                        ReadmeOptionPair pair = hardGet(url);
+                        readmes.insert(new BasicDBObject("readme",
+                                ReadmeCorrelations.getCorrelations(pair.getReadme()))
+                                .append("readme_name", pair.getName())
+                                .append("name", repoObject.get("name")));
+                    }
+
+                });
+            }
+        } catch (Throwable t) {
+            t.printStackTrace();
+        } finally {
+            repoCursor.close();
+            exec.shutdown();
+        }
+    }
+
+    private static ReadmeOptionPair hardGet(String url) {
+        Http http = new Http();
+        String readme;
+        for (String possibility : POSSIBLE_READMES) {
+            try {
+                readme = http.get(url + possibility);
+                if (!"Not Found".equals(readme)) {
+                    return new ReadmeOptionPair(readme, possibility);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                Logger.warn("The possibility " + possibility + " is not vaild for "
+                + url);
+            }
+        }
+        // default so we don't have to deal with null strings
+        return new ReadmeOptionPair("", "");
+    }
+
+    private static String easyGet(String url) {
+        try {
+            return new Http().get(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Logger.fatal("IOException, was unable to easyGet url " + url);
+            return "";
+        }
+    }
+
+    private static String getURL(BasicDBObject object) {
+        return README_BASE_URL + object.get("name") + object.get("branch")
+                + "/";
+    }
+}
---
+++ FetchAndUpdate/src/io/kare/suggest/readmes/ReadmeCorrelations.java
@@ -0,0 +1,53 @@
+package io.kare.suggest.readmes;
+
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Adrian Chmielewski-Anders
+ * @since 0.0.1
+ */
+
+public class ReadmeCorrelations {
+
+    private static ArrayList<String> tags = null;
+
+    public static List<String> getCorrelations(String readme) {
+        String[] words = removeChars(readme);
+
+        if (tags == null) {
+            initializeTags();
+        }
+        List<String> keywords = new ArrayList<>();
+        Arrays.stream(words).forEach((String s) -> {
+            if (tags.contains(s)) {
+                keywords.add(s);
+            }
+        });
+        return keywords;
+    }
+
+    private static void initializeTags() {
+        // todo: fix this path so it's the right location for tags.csv
+        try {
+            new BufferedReader(new FileReader("tags.csv")).lines().forEach((s) -> {
+                Collections.addAll(tags, s.split(", "));
+            });
+        } catch (FileNotFoundException ignored) {}
+    }
+
+    private static String[] removeChars(String readme) {
+        // main regex, will remove all of the markdown specific characters
+        // while preserving links, also removes a lot of the random chars like
+        // ,.=-*<> etc.
+        return readme.replaceAll("[^A-Za-z0-9\\.&&[(.+) ?+\\[.+\\]]]|[#?+]|[`?"
+                + "+]|[<|>|**|*|-|,|=|!]", " ").replaceAll("\\s+", "")
+                .toLowerCase().trim().split(" ");
+    }
+}
