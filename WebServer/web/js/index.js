e82110459cc7a9c977431f9c2944efc4229b2e67
Ritwik Dutta <ritzymail@gmail.com>
2014-06-04 05:54:56 -0700

    Now, input can be provided to the function in the form "user/repo", to simplify the backend.

--- WebServer/web/js/index.js
+++ WebServer/web/js/index.js
@@ -1,18 +1,18 @@
 var converter = new Showdown.converter();
-var results = new Array();
-
-$(".page-header").text("");
-
 var keys = new Array();
 
 var addElem = function(key) {
-	var li = '<li style = "opacity: 0;" id = "' + key[0]+key[1] + '" class = "result"><a href="#" class = "'+ key[0]+key[1] +'">' + key[0] + "/" + key[1] + '</a>'+
-	'<a class = "dir-link" href="https:/github.com/' + key[0] + "/" + key[1]+
+
+	var id = key.split("/").join("");
+	var li = '<li style = "opacity: 0;" id = "' + id + '" class = "result"><a href="#" class = "'+ id +'">' + key + '</a>'+
+	'<a class = "dir-link" href="https:/github.com/' + key +
 	'"><img class = "view-icon" src = "assets/github.png"></a></li>"';
 	$("#results").append(li);
+	
 	keys.push(key);
-	$("#" + key[0]+key[1]).on("click", function() {
-		var text = $("." + key[0]+key[1]).text();
+
+	$("#" + id).on("click", function() {
+		var text = $("." + id).text();
 		console.log(text);
 		$("#readme").fadeOut(50,
 			function() {
@@ -28,7 +28,9 @@ var addElem = function(key) {
 
 var popRes = function(resKeys) {
 	for (var i = 0; i < resKeys.length; i++) {
+		var id = resKeys[i].split("/").join("");
+		console.log(resKeys[i]);
 		addElem(resKeys[i]);
-		$("#" + resKeys[i][0] + resKeys[i][1]).animate( {"opacity": "1"}, 50);
+		$("#" + id).animate( {"opacity": "1"}, 50);
 	}
 };
