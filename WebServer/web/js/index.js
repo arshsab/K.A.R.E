08145c64a438f5421d08ad8d6a85b0de00937369
Ritwik Dutta <ritzymail@gmail.com>
2014-06-04 05:48:15 -0700

    Finished large amount of frontend.

--- WebServer/web/js/index.js
+++ WebServer/web/js/index.js
@@ -1,13 +1,34 @@
-var setVis = function (opacity) {
-	$(".page-header").css("opacity", opacity);
-}
+var converter = new Showdown.converter();
+var results = new Array();
 
+$(".page-header").text("");
 
-$(".result").on("click", function() {
-	setVis("1");
-	var text = $(this).text();
-	$(".page-header").fadeOut(200,
-		function() {
-			$(".page-header").text(text).fadeIn(200);
-		});
-});
+var keys = new Array();
+
+var addElem = function(key) {
+	var li = '<li style = "opacity: 0;" id = "' + key[0]+key[1] + '" class = "result"><a href="#" class = "'+ key[0]+key[1] +'">' + key[0] + "/" + key[1] + '</a>'+
+	'<a class = "dir-link" href="https:/github.com/' + key[0] + "/" + key[1]+
+	'"><img class = "view-icon" src = "assets/github.png"></a></li>"';
+	$("#results").append(li);
+	keys.push(key);
+	$("#" + key[0]+key[1]).on("click", function() {
+		var text = $("." + key[0]+key[1]).text();
+		console.log(text);
+		$("#readme").fadeOut(50,
+			function() {
+				var content;
+				$.get("https://api.github.com/repos/"+text +"/readme", 
+					function(e) { 
+						content = atob(e.content); 
+						$("#readme").html(converter.makeHtml(content)).fadeIn(50);
+					});
+			});
+	});
+};
+
+var popRes = function(resKeys) {
+	for (var i = 0; i < resKeys.length; i++) {
+		addElem(resKeys[i]);
+		$("#" + resKeys[i][0] + resKeys[i][1]).animate( {"opacity": "1"}, 50);
+	}
+};
