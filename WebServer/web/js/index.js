904800e35b99c0d72a57d08b5e02f767b559712c
Adrian Chmielewski-Anders <a@pyza.net>
2014-06-07 20:15:55 -0700

    finishing up the stuff

--- WebServer/web/js/index.js
+++ WebServer/web/js/index.js
@@ -1,43 +1,45 @@
 var converter = new Showdown.converter();
-var keys = new Array();
+var keys = [];
 
 var addElem = function(key) {
 
-	var id = key.split("/").join("");
-	var li = '<li style = "opacity: 0;" id = "' + id + '" class = "result"><a href="#" class = "'+ id +'">' + key + '</a>'+
-	'<a class = "dir-link" href="https:/github.com/' + key +
-	'"><img class = "view-icon" src = "assets/github.png"></a></li>"';
-	$("#results").append(li);
-	
-	keys.push(key);
+    var id = key.split("/").join("");
+    var li = '<li style = "opacity: 0;" id = "' + id + '" class = "result"><a href="#" class = "'+ id +'">' + key + '</a>'+
+    '<a class = "dir-link" href="https:/github.com/' + key +
+    '"><img class = "view-icon" src = "assets/github.png" align="bottom"></a></li>"';
+    $("#results").append(li);
 
-	$("#" + id).on("click", function() {
-		var text = $("." + id).text();
-		console.log(text);
-		$("#readme").fadeOut(50,
-			function() {
-				var content;
-				$.get("https://api.github.com/repos/"+text +"/readme", 
-					function(e) { 
-						content = atob(e.content); 
-						$("#readme").html(converter.makeHtml(content)).fadeIn(50);
-					});
-			});
-	});
+    keys.push(key);
 
-	$("#" + id).animate( {"opacity": "1"}, 50);
+    $("#" + id).on("click", function() {
+        var text = $("." + id).text();
+        console.log(text);
+        $("#readme").fadeOut(50,
+            function() {
+                var content;
+                $.get("https://api.github.com/repos/"+text +"/readme", 
+                    function(e) {
+                        console.log(e);
+                        console.log(e.content);
+                        content = atob(e.content); 
+                        $("#readme").html(converter.makeHtml(content)).fadeIn(50);
+                    });
+            });
+    });
+
+    $("#" +  id).animate( {"opacity": "1"}, 50);
 };
 
 var popRes = function(resKeys) {
-	for (var i = 0; i < resKeys.length; i++) {
-		var id = resKeys[i].split("/").join("");
-		console.log(resKeys[i]);
-		addElem(resKeys[i]);
-	}
+    for (var i = 0; i < resKeys.length; i++) {
+        var id = resKeys[i].split("/").join("");
+        console.log(resKeys[i]);
+        addElem(resKeys[i]);
+    }
 };
 
 var getResult = function() {
-	window.location.href = "results.html#" + $("#search").val();
+    window.location.href = "results.html#" + $("#search").val();
 };
 
 $("#search").keyup(function(event){
@@ -52,9 +54,12 @@ $("#search-box").keyup(function(event){
     }
 });
 
+var getParams = function() {
+    return document.location.search.replace(/(^\?)/,'').split('&').reduce(function(o,n){n=n.split('=');o[n[0]]=n[1];return o},{});
+};
+
+
 $(document).ready(function() {
-	if (window.location.hash.split("/").length > 1) {
-		var repo = window.location.hash.substring(1);
-		addElem(repo);
-	}
+    var query = decodeURIComponent(getParams()["search"]);
+        addElem(query);
 });
