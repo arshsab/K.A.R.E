4dd15b1e0c22f441eba828fb8abfd06914bd61f9
Ritwik Dutta <ritzymail@gmail.com>
2014-06-08 22:57:27 -0700

    Misc changes.

--- WebServer/src/io/kare/server/Main.java
+++ WebServer/src/io/kare/server/Main.java
@@ -1,15 +1,13 @@
 package io.kare.server;
 
-import com.mongodb.DB;
-import com.mongodb.MongoClient;
-import io.kare.server.recommend.Recommender;
 import io.kare.server.servlets.FileRoute;
 import io.kare.server.servlets.LinearRecommendationsRoute;
 import spark.Spark;
 
 import java.io.File;
 
-import static spark.Spark.*;
+import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
 
 /**
  * @author arshsab
@@ -23,13 +21,14 @@ public class Main {
         if (args.length != 0)
             Spark.setPort(Integer.parseInt(args[0]));
         else
-            Spark.setPort(80);
+            Spark.setPort(8081);
 
-        staticFileLocation("web");
+        staticFileLocation("/web");
 
         get("/search/:repo/:owner", new FileRoute("web/results.html"));
         get("/index", new FileRoute("web/index.html"));
+        get("/", new FileRoute("web/index.html"));
 
-        get("/search_json", new LinearRecommendationsRoute());
+        get("/search_json/:repo/:owner", new LinearRecommendationsRoute());
     }
 }
