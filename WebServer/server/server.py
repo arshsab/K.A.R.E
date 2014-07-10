91d4edc5cfaf019d066fd1cbf4266998828f4314
Arshdeep Sabharwal <arshdeepsabharwal14@mittymonarch.com>
2014-07-09 18:30:17 -0700

    Server done.

--- WebServer/server/server.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__, static_url_path='')
-app.config.from_object(__name__)
-
-from views import *
-
-if __name__ == '__main__':
-    app.run()
--- WebServer/server/server.wsgi
+++
@@ -1,5 +0,0 @@
-import sys
-
-sys.path.insert(0, '/var/www/server')
-
-from server import app as application
